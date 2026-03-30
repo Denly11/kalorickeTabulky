6778be04220c5816b9e8cbb637654d8b6a7cbbcc
denis <deniskovaci@seznam.cz>
2026-03-30 22:29:24 +0200

    connection fixed

--- kaloricke_tabulky_backend/database.py
+++ kaloricke_tabulky_backend/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine#nativní spojení s databází
-from sqlalchemy.orm import sessionmaker, declarativeBase#sessionmaker pro CRUD operace, declarativeBase pro definici tabulek
+from sqlalchemy.orm import sessionmaker, DeclarativeBase#sessionmaker pro CRUD operace, DeclarativeBase pro definici tabulek
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/kaloricke_tabulky"
+DATABASE_URL = "postgresql://postgres:psswd123@localhost:5432/kaloricke_tabulky"
+#
 
-class Base(declarativeBase):
+class Base(DeclarativeBase):
     pass # třída pro definici tabulek, dědí z declarativeBase, ale zatím není potřeba nic přidávat
 
 engine = create_engine(
